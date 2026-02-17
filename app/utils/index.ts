aa6b0db866b4a93102782bfccdbad3201d912a59
Zadigo <pendenquejohn@gmail.com>
2026-02-17 15:07:34 +0100

    Check for undefined

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -7,6 +7,6 @@ import type { Undefineable } from '~/types'
  * @param parts The parts to be added to the id, usually the props of the component
  */
 export function createElementId(base: string, parts: Array<Undefineable<string | number>> = [], join = '__') {
-  const _parts = parts.map(p => (p || '').toString().replace(' ', '-')).filter(Boolean).join(join)
-  return `${base}${join + _parts}`
+  const _parts = parts ? parts.map(p => (p || '').toString().replace(' ', '-')).filter(Boolean).join(join) : ''
+  return `${base}${(join + _parts).trim()}`
 }
